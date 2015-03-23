817b0560e7e9c58a86cb734b406ca563d22f5449
Connor Ring <connorring7778@sjccs142.saintjoe.edu>
2015-03-23 10:11:25 -0400

    Initial commit

---
+++ parser.js
@@ -0,0 +1,24 @@
+//Connor Ring Parse Exercise
+//March 23, 2015
+
+function parseINI(string) {
+	  //Start with an object to hold the top-level fields
+	  var currentSection = {name: null, fields: []};
+	  var categories = [currentSection];
+	  
+	  string.split(/\r?\n/).forEach(function(line) {
+	    var match;
+	    if (/^\s*(;.*)?$/.test(line)) {
+	      return;
+	    } else if (match = line.match(/^\[(.*)\]$/)) {
+	      currentSection = {name: match[1], fields: []};
+	      categories.push(currentSection);
+	    } else if (match = line.match(/^(\w+)=(.*)$/)) {
+	      currentSection.fields.push({name: match[1],
+	                                  value: match[2]});
+	    } else {
+	      throw new Error("Line '" + line + "' is invalid.");
+	    }
+	  });
+	  return categories;
+}
